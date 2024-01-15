8fe9e1d94d78b68131d1e3f418fcfa4efa72dc37
koisose.eth <plokotokbelajar@gmail.com>
2024-01-15 13:17:04 +0000

    🔧 fix(scripts): ubah perintah push untuk paksa update ke branch utama
    Mengubah perintah `git push -u origin main` menjadi `git push -u origin main --force` untuk memaksa pembaruan pada branch utama ketika perubahan di-commit.

--- packages/scripts/commit-clarifai.js
+++ packages/scripts/commit-clarifai.js
@@ -119,7 +119,7 @@ async function gitDiffStaged() {
     //   console.log(text3)
       execSync(`git add -A`);
       execSync(`printf "${text4.replace(/```/g, '')}" | git commit -F-`);
-      execSync("git push -u origin main");
+      execSync("git push -u origin main --force");
       process.exit();
     } catch (e) {
       console.log(e.message);
